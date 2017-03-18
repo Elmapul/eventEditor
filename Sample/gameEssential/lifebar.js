8914678386d94425acddd38f87c8d32142e6ba99
Elmapul <igccpao@gmail.com>
2017-03-18 05:21:28 -0300

    Create lifebar.js

---
+++ Sample/gameEssential/lifebar.js
@@ -0,0 +1,52 @@
+//tela
+var c = document.getElementById("Canvas");
+var ctx = c.getContext("2d");
+
+
+//lifebar.draw();
+//tela
+
+//barra de vida
+var Bar = function(name, x,y,max) {
+    this.name = name;
+    this.x = x;
+    this.y = y;
+    this.max=max;
+    this.current=max;
+    this.size=100;
+}
+
+Bar.prototype = {
+    setCurrent: function(value) {
+        this.current = value;
+    },
+    draw: function(){
+    this.area=this.size * (this.current /this.max);
+			ctx.beginPath();
+			ctx.rect(this.x, this.y, this.x+this.area, this.y+10);
+			ctx.fillStyle = "rgba(0,255,0,1)";
+      ctx.strokeStyle="rgba(0,0,0,0)";
+      ctx.closePath();
+			ctx.fill();
+      
+      ctx.beginPath();
+      
+			ctx.rect(this.x, this.y, this.x+this.size, this.y+10);
+      ctx.strokeStyle="rgba(0,0,0,1)";
+      ctx.closePath();
+			ctx.stroke();
+}
+    
+    
+    
+    
+}
+//barra de vida.
+
+
+var bar = new Bar('life', 10,10,100);
+//bar.setCurrent(10);
+console.log(bar.name);
+console.log(bar.current);
+bar.setCurrent(90);
+bar.draw();
